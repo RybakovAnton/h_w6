02ba3dfb9e6cfd3828f812286a43d9ce5caedbdc
Anton <antonrybakov95@yandex.ru@yandex.ru>
2023-07-26 21:52:51 +0800

    Задача 41

---
+++ z41/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+System.Console.WriteLine("Введите колличество чисел");
+int m = int.Parse(Console.ReadLine());
+
+int count = 0;
+
+for (int i = 0; i < m; i++)
+{
+    System.Console.WriteLine($"Введите число {i + 1}: ");
+    int num = int.Parse(Console.ReadLine());
+    
+    if (num>0)
+    {
+        count++;
+    }
+}
+System.Console.WriteLine($"Кол-во чисел больше нуля: "+ count);
+
